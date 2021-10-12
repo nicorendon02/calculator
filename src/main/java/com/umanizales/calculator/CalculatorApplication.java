8d684127cfb291a6e0cf1f14d9aae97f353b3123
nicorendon02 <nrendon87351@umanizales.edu.co>
2021-10-12 00:23:17 -0500

    CORS configurer Implemented

--- src/main/java/com/umanizales/calculator/CalculatorApplication.java
+++ src/main/java/com/umanizales/calculator/CalculatorApplication.java
@@ -2,6 +2,9 @@ package com.umanizales.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CalculatorApplication {
@@ -10,4 +13,14 @@ public class CalculatorApplication {
         SpringApplication.run(CalculatorApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                        .allowedMethods("/*").allowedHeaders("*");
+            }
+        };
+    }
 }
